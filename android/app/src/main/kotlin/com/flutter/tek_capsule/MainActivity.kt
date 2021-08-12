16553f90a18cb07a25aa6dcf3db9ffb6fa15ec0e
karthik <karthikeyanvijayaraja@gmail.com>
2021-08-12 21:43:20 +0530

    first draft

--- android/app/src/main/kotlin/com/flutter/tek_capsule/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.flutter.tek_capsule
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/themes/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color whiteTextColor = Colors.white;
-Color lightScaffoldBackgroundColor = Color(0xffF8FCFF);
-Color iconColor = Colors.white;
