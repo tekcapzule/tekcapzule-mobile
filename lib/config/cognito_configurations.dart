f2fa77a9aecfeee112d87572b95479ac8215685a
kartekspace <karthikeyanvijayaraja@gmail.com>
2023-02-09 23:08:54 +0530

    Cognito secrets removed.

--- lib/config/cognito_configurations.dart
+++ lib/config/cognito_configurations.dart
@@ -9,10 +9,10 @@ const cognito_configurations = ''' {
             "awsCognitoAuthPlugin": {
                 "CognitoUserPool": {
                     "Default": {
-                       "PoolId": "us-east-2_CtZ1ShfHp",
-                        "AppClientId": "3dvc885p65n8h0iptadvkd4g4s",
+                       "PoolId": "",
+                        "AppClientId": "",
                         "Region": "us-east-2",
-                        "AppClientSecret": "e4ujbglssiinuhhpmqr21674grqlq876jlrsc5e9llfle0m63tm"
+                        "AppClientSecret": ""
                     }
                 },
                 "Auth": {
